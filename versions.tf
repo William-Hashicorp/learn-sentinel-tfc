81cab23f660d39fe06b55b70c0f3dc62f97bb876
root <root@William-SB2.localdomain>
2022-01-10 20:14:06 +0800

    change org name

--- versions.tf
+++ versions.tf
@@ -13,7 +13,7 @@ terraform {
   required_version = ">= 0.15"
 
   backend "remote" {
-    organization = "<YOUR_TERRAFORM_ORG>"
+    organization = "William-Hashicorp"
 
     workspaces {
       name = "sentinel-example"
